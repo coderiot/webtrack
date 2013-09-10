6d14607bcda04481211a98f984628aed53f5e5f1
peterrr <peterrr@github>
2013-09-10 14:09:51 +0200

    add license in setup.py.

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from distutils.core import setup
 
 setup(name='webtrack',
     version='0.1.0',
+    license="GPLv2",
     description='Keep track of updates from websites.',
     author='peterrr',
     url='https://github.com/peterr/webtrack/',
